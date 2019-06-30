9036fa2c0f65512ea0d2052c0d86d3ce24b58ef1
johnbryant <johnbryant262@gmail.com>
2019-06-30 16:39:24 -0400

    Updates

---
+++ precache-manifest.0437fdaa79faea2f2fb88390912a901b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12408fdd0d38920603987c9d703e86e",
+    "url": "/github-battle/index.html"
+  },
+  {
+    "revision": "19d1033db0a8daabc7c2",
+    "url": "/github-battle/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/github-battle/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "19d1033db0a8daabc7c2",
+    "url": "/github-battle/static/js/main.751c9303.chunk.js"
+  },
+  {
+    "revision": "f311d96d74bdc16e8f8c",
+    "url": "/github-battle/static/js/runtime~main.6176a14e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-battle/static/media/logo.5d5d9eef.svg"
+  }
+]);
