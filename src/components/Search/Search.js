db2f6b12beb6ae04f93cef90163000f938ef5d0d
johnbryant <johnbryant262@gmail.com>
2019-07-09 16:52:41 -0400

    Search -- basic UI

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "../../style/Search.css";
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div>This is Search</div>;
+    return (
+      <div>
+        <form className="searchForm">
+          <label className="searchLabel">Enter a github username</label>
+          <input type="text" placeholder="github username" />
+          <button className="searchButton" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="profile">
+          <div className="profileInfo">Info</div>
+          <div className="profileRepo">Repo</div>
+        </div>
+      </div>
+    );
   }
 }
 
